560c61831359c44cae582252bb57a4d97b31a5f1
paapu68 <markus.kaukonen@iki.fi>
2017-04-03 21:31:50 +0300

    almost there with rectangles

--- getCharacterRegions.py
+++ getCharacterRegions.py
@@ -64,23 +64,63 @@ class GetCharacterRegions():
                 ok.append((x,y,w,h))
         self.regions = ok
 
+    def checkSameness(self, toleranceInPixel=3):
+        """ if we have almost the same rectange, kill one of them
+            tthe remaining rectangle is as big as possible
+        """
+        deletes=[]
+        for i, region_i in enumerate(self.regions):
+            delete_i = False
+            print("A", i, region_i)
+            start=i+1
+            for j in range(start, len(self.regions)):
+                [x1,y1,w1,h1] = region_i
+                [x2,y2,w2,h2] = self.regions[j]
+                if ((abs(x2-x1) < toleranceInPixel) and \
+                    (abs(y2-y1) < toleranceInPixel) and \
+                    (abs(w2-w1) < toleranceInPixel) and \
+                    (abs(h2-h1) < toleranceInPixel)):
+                    delete_i = True
+                    print(self.regions[j][0], min(x1,x2))
+                    self.regions[j][0]=min(x1,x2)
+                    self.regions[j][1]=min(y1,y2)
+                    self.regions[j][2]=max(w1,w2)
+                    self.regions[j][3]=max(h1,h2)
+            deletes.append(delete_i)
+        ok = []
+        for delete, region in zip(deletes,self.regions):
+            if not delete:
+                ok.append(region)
+        self.regions = ok
+
     def getSetsOfSix(self, heightCriterium=0.05):
         """ get all possible 6-sets of letters that are close in height"""
+        import itertools
         self.listOfSixSets = []
         mymin=1-heightCriterium
         mymax=1+heightCriterium
         for i in range(len(self.regions)):
             heightI=self.regions[i][3]
-            sixSet=set([self.regions[i]])
-            print('ini:', sixSet)
-            for j in range(i+1,len(self.regions)):
+            sixSet=set(self.regions[i])
+            #print('ini:', sixSet)
+            for j in range(len(self.regions)):
                 heightJ=self.regions[j][3]
+                #print(i, j, heightJ/heightI)
                 if (mymin < heightJ/heightI) and (mymax > heightJ/heightI):
                     sixSet.add(self.regions[j])
             # check that number of rectangles is six and this set of rectangles is new
-            if len(sixSet) == 6 and not(sixSet in self.listOfSixSets):
-                print("sixset: ", sixSet)
-                self.listOfSixSets.append(sixSet)
+            #print("I, LEN SET",i, len(sixSet))
+        if len(sixSet) == 6 and not(sixSet in self.listOfSixSets):
+            #print("sixset: ", sixSet)
+            self.listOfSixSets.append(sixSet)
+            # if we have a longer set, take all 6-permutations..
+        elif len(sixSet) > 6:
+            myFixedSet = frozenset(sixSet)
+            sixSetTrials= itertools.permutations(myFixedSet, 6)
+            for addSet in sixSetTrials:
+                if not(set(addSet) in self.listOfSixSets):
+                    self.listOfSixSets.append(set(addSet))
+
 
     def sortSetsAndToList(self):
         """sort sets by x coordinate"""
@@ -104,10 +144,40 @@ class GetCharacterRegions():
 
             self.listOfSixLists.append(sorted)
 
-    def checkXcloseness(self):
-        """check that subsegueent rectangles are close enought"""
+    def checkSixXcloseness(self, minFraction=1.0, maxFraction=1.5):
+        """check that subsegueent rectangles are close enought
+            if NOT remove the 6-rectangle """
+        import numpy as np
 
+        deleteList = []
+        for rectangles in self.listOfSixLists:
+            averageWidth=np.average(np.asarray(rectangles[:][0]))
+            if ((rectangles[1][0] - rectangles[0][0]) < (minFraction * averageWidth) or \
+                (rectangles[2][0] - rectangles[1][0]) < (minFraction * averageWidth) or \
+                (rectangles[3][0] - rectangles[2][0]) < (minFraction * averageWidth) or \
+                (rectangles[4][0] - rectangles[3][0]) < (minFraction * averageWidth) or \
+                (rectangles[5][0] - rectangles[4][0]) < (minFraction * averageWidth) or \
+                (rectangles[1][0] - rectangles[0][0]) > (maxFraction * averageWidth) or \
+                (rectangles[2][0] - rectangles[1][0]) > (maxFraction * averageWidth) or \
+                #(rectangles[3][0] - rectangles[2][0]) > (maxFraction*averageWidth) or \ not for '-'
+                (rectangles[4][0] - rectangles[3][0]) > (maxFraction * averageWidth) or \
+                (rectangles[5][0] - rectangles[4][0]) > (maxFraction * averageWidth)):
+                deleteList.append(True)
+            else:
+                deleteList.append(False)
+        accepted = []
+        for i, delete in enumerate(deleteList):
+            if not delete:
+                accepted.append(self.listOfSixLists[i])
 
+    def showRectangles(self):
+        clone = self.getClone()
+        for (x,y,w,h) in self.regions:
+            cv2.rectangle(clone,(x,y),(x+w,y+h),(0,255,0),5)
+            roi_gray = clone[y:y+h, x:x+w]
+        cv2.imshow('clone', clone)
+        while(cv2.waitKey()!=ord('q')):
+            continue
 
 if __name__ == '__main__':
     import sys
@@ -116,22 +186,34 @@ if __name__ == '__main__':
     app.checkHeight()
     app.checkWidth()
     app.checkArea()
+    print("all RECTANGLEs ",app.regions)
+    print("end of all RECTANGLES")
+    app.checkSameness()
+    print("22: all RECTANGLEs ",app.regions)
+    print("22: end of all RECTANGLES")
+    sys.exit()
     app.getSetsOfSix()
-    print("LIST OF RECTANGLE CANDIDATES",app.listOfSixSets)
-    print("end of candidates")
+    #print("LIST OF RECTANGLE CANDIDATES",app.listOfSixSets)
+    #print("end of candidates")
     app.sortSetsAndToList()
     print("LIST OF SORTED RECTANGLE CANDIDATES",app.listOfSixLists)
     print("end of SORTED candidates")
-    for region in app.regions:
-        print(region)
+    app.showRectangles()
+    app.checkSixXcloseness()
+    print("FINAL LIST OF SORTED RECTANGLE CANDIDATES",app.listOfSixLists)
+    print("end of SORTED candidates")
+
+    #for region in app.regions:
+    #    print(region)
 
     clone = app.getClone()
     #for six in app.listOfSets:
     #    for rectangle in six:
 
-    for i, [x,y,w,h] in enumerate(app.regions):
-        cv2.rectangle(clone,(x,y),(x+w,y+h),(0,255,0),5)
-        roi_gray = clone[y:y+h, x:x+w]
+    for sixList in app.listOfSixLists:
+        for i, [x,y,w,h] in enumerate(sixList):
+            cv2.rectangle(clone,(x,y),(x+w,y+h),(0,255,0),5)
+            roi_gray = clone[y:y+h, x:x+w]
     #        #roi_color = img[y:y+h, x:x+w]
     #        cv2.imwrite(str(i)+'-'+sys.argv[1]+'.jpg', roi_gray)
 
